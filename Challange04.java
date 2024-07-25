21579bc71c8f9551866de802a87380a9e3381d20
Rashmipatel044 <115832496+Rashmipatel044@users.noreply.github.com>
2024-07-25 07:10:48 +0530

    updating 04

--- Challange04.java
+++ Challange04.java
@@ -1,7 +1,18 @@
-package Java;
 
+//Converting Decimal Number into Binary
+import java.util.Scanner;
 public class Challange04 {
     public static void main(String[] args) {
         System.out.print("Repo Test");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your Decimal number");
+        int a =sc.nextInt();
+        while(a!=0){
+            int r=a%2;
+            a=a/2;
+            System.out.print(r);
+        }
+
+sc.close();
     }
 }
